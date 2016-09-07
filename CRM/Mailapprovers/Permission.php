c6346bea7809d718b83b37329d1b295a7a211684
Francis Whittle <francis@agileware.com.au>
2016-09-07 12:23:32 +1000

    Implement permissions check for Approval page.

--- CRM/Mailapprovers/Permission.php
+++ CRM/Mailapprovers/Permission.php
@@ -1,8 +1,77 @@
 <?php
-
+/**
+ * Overrides certain permission checks in CiviCRM based on configrured Group
+ * membership.
+ */
 class CRM_Mailapprovers_Permission extends CRM_Core_Permission_Temp {
+  private $wrapped;
 
-  function check() {
-    CRM_Core_Session::setStatus('Custom "Temp" Check', CRM_Mailapprovers_Permission);
+  private $arg;
+
+  function __construct($arg, $wrapping = NULL) {
+    $this->arg = $arg;
+
+    if(is_a($wrapping, 'CRM_Core_Permission_Temp')) {
+      $this->wrapped = $wrapping;
+    }
+  }
+
+  function check($permission) {
+    // We only care about the 'approve mailings' permission.
+    if ($permission != 'approve mailings') {
+      return ($this->wrapped ? $this->wrapped->check($permission) : FALSE);
+    }
+
+    // Pretend we can approve anything on the browse URL
+    if ($this->arg[2] == 'browse') {
+      return TRUE;
+    }
+
+    // Mailing ID - must exist in request.
+    $mid = CRM_Utils_Request::retrieve('mid', 'Positive');
+
+    if (!$mid) {
+      return FALSE;
+    }
+
+    try {
+      // Get the from_email address of the email in question
+      $mailing = civicrm_api('Mailing', 'getsingle', array('version' => '3', 'id' => $mid, 'return' => 'from_email'));
+
+      // Speculate on the "From Email" ID based on the address.  This may return multiple results.
+      $emails = civicrm_api('OptionValue', 'get', array(
+                  'version' => '3',
+                  'option_group_id' => 'from_email_address',
+                  'domain_id' => CRM_Core_Config::singleton()->domainID(),
+                  'label' => array('LIKE' => "%<{$mailing['from_email']}>%"),
+                  'return' => 'value',
+                ));
+
+      // Get the list of approvers.
+      $approvers = Civi::settings()->get('mail_approvers');
+
+      // Get logged in user's groups as an array
+      $groups = civicrm_api3('Contact', 'getvalue', array(
+                  'id' => CRM_Core_Session::singleton()->getLoggedInContactID(),
+                  'return' => 'groups'
+                ));
+
+      if($groups) {
+        $groups = explode(',', $groups);
+      }
+      else {
+        $groups = array();
+      }
+
+      // Loop through all possible approver lists and return TRUE as soon as one matches or the list is empty.
+      foreach($emails['values'] as $e) {
+        if(empty($approvers[$e['value']]) || !empty(array_intersect($approvers[$e['value']], $groups))) {
+          return TRUE;
+        }
+      }
+    }
+    catch (Exception $e) { }
+
+    return FALSE;
   }
 }
