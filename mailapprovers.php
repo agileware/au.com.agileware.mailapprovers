c8e9e807f44ba87e566b7568221c0e4fd026891c
agileware-justin <agileware-justin@users.noreply.github.com>
2026-09-12 09:11:41 +0000

    TASK-2296 [AUTO] civix upgraded
    Files changed:
    M	info.xml
    M	mailapprovers.civix.php
    M	mailapprovers.php
    A	mixin/smarty-v2@1.0.3.mixin.php

--- mailapprovers.php
+++ mailapprovers.php
@@ -27,17 +27,6 @@ function mailapprovers_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mailapprovers_civicrm_xmlMenu(&$files) {
-  _mailapprovers_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -47,96 +36,12 @@ function mailapprovers_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mailapprovers_civicrm_uninstall() {
-  _mailapprovers_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function mailapprovers_civicrm_enable() {
   _mailapprovers_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mailapprovers_civicrm_disable() {
-  _mailapprovers_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mailapprovers_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mailapprovers_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mailapprovers_civicrm_managed(&$entities) {
-  _mailapprovers_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailapprovers_civicrm_caseTypes(&$caseTypes) {
-  _mailapprovers_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailapprovers_civicrm_angularModules(&$angularModules) {
-_mailapprovers_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mailapprovers_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mailapprovers_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -251,9 +156,8 @@ function mailapprovers_civicrm_pre($op, $objectName, $objectId, &$params) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function mailapprovers_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
